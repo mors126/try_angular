c2eb4b2e21593dbcceca3eb12f3d2dcfdf3a53d1
Maksym Moroz <mors126@mail.ru>
2015-10-23 21:52:43 +0300

    Addin main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /*global angular */
 var myApp = angular.module('myApp', []);
 
+//comment is here ...
+
 /*global console */
 myApp.controller('mainController', function ($scope) {
     "use strict";
